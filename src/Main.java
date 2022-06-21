8847cbbdff8b34adc84a3c096779b966dc624e2e
Lucas Lima <lucas.lima@cisbrasil.local>
2022-06-21 18:59:37 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double n1;
+        double n2;
+        double n3;
+        double n4;
+        double media;
+
+        n1 = 7;
+        n2 = 8;
+        n3 = 9;
+        n4 = 10;
+
+        media = (n1 + n2 + n3 + n4) / 4;
+
+        System.out.println("Media: " + media);
+    }
+}
